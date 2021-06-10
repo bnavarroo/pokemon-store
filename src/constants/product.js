b35c5509da794caa338cb9eab2ba546df9cddee0
bnavarroo <bruno.navarro91@hotmail.com>
2021-06-10 19:26:11 -0300

    Alterada imagem de not found

--- src/constants/product.js
+++ src/constants/product.js
@@ -1,8 +1,10 @@
+import NotAvailableImg from '../_assets/img/not-available.png';
+
 export const PRODUCT_IMAGE_SETTINGS = {
   strReplace: '{id}',
   url: 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/{id}.svg',
 };
 
-export const PRODUCT_IMAGE_NOTFOUND = 'https://static.quizur.com/i/b/57c1c26fc0b812.5998420157c1c26fb156c9.51498011.png';
+export const PRODUCT_IMAGE_NOTFOUND = NotAvailableImg;
 
 export default {};
