aa8b4b092df8fe98b908b095efccccf904afa95c
bnavarroo <bruno.navarro91@hotmail.com>
2021-06-24 11:49:27 -0300

    - Resolução de caminho relativo de pastas - Exclusão de arquivos não utilizados

--- src/index.js
+++ src/index.js
@@ -1,9 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
-import Store from './_config/store';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import Store from '~/_config/store';
+import App from '~/App';
 
 ReactDOM.render(
   <Provider store={Store}>
@@ -11,8 +10,3 @@ ReactDOM.render(
   </Provider>,
   document.getElementById('root'),
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
